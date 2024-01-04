43d27a55be6ca198ed476705673291ee5edb949e
Basile Carle <basile.carle@edhec.com>
2024-01-04 20:17:53 +0100

    test config cors différente

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,56 +10,27 @@ const app = express();
 
 // use some application-level middlewares
 
-// const whitelist = [
-//   process.env.FRONTEND_URL,
-//   process.env.FRONTEND_URL_WWW,
-//   process.env.FRONTEND_URL_local_WWW,
-// ];
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   optionsSuccessStatus: 200,
-//   credentials: true,
-// };
-
-// app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+    // credentials: true,
+  })
+);
 
 // app.use(
 //   cors({
-//     origin:
-//       [process.env.FRONTEND_URL, process.env.FRONTEND_URL_WWW] ??
+//     origin: [
+//       "http://192.168.31.10:3002",
+//       "http://192.168.31.84:3002",
 //       "http://localhost:3002",
+//       "http://tablepartage.ddns.net:81",
+//     ],
+
 //     optionsSuccessStatus: 200,
 //     credentials: true,
 //   })
 // );
-
-// app.use(
-//   cors({
-//     origin: "*",
-//     optionsSuccessStatus: 200,
-//     // credentials: true,
-//   })
-// );
-
-app.use(
-  cors({
-    origin: [
-      "http://192.168.31.10:3002",
-      "http://192.168.31.84:3002",
-      "http://localhost:3002",
-      "http://tablepartage.ddns.net:81",
-    ],
-
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
 
 app.use(express.json());
 app.use(cookieParser());
